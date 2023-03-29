e28a824b19636c97c44e3edf85c0d152dd89846e
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-03-29 17:36:47 +0200

    feat: Move bank and wasm modules to test-tube
    feat: Export modules from test-tube

--- packages/test-tube/src/module/wasm.rs
+++ packages/test-tube/src/module/wasm.rs
@@ -6,9 +6,9 @@ use cosmrs::proto::cosmwasm::wasm::v1::{
 use cosmwasm_std::Coin;
 use serde::{de::DeserializeOwned, Serialize};
 
-use test_tube::runner::error::{DecodeError, EncodeError, RunnerError};
-use test_tube::runner::result::{RunnerExecuteResult, RunnerResult};
-use test_tube::{
+use crate::runner::error::{DecodeError, EncodeError, RunnerError};
+use crate::runner::result::{RunnerExecuteResult, RunnerResult};
+use crate::{
     account::{Account, SigningAccount},
     runner::Runner,
 };

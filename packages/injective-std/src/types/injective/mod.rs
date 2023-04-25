60d6702e78af084bc7f6d551d85c9644104fa72e
Max <max.grabner@staedter.io>
2023-04-25 21:36:58 +0100

    removed useless modules for INJ

--- packages/injective-std/src/types/injective/mod.rs
+++ packages/injective-std/src/types/injective/mod.rs
@@ -1,11 +1,6 @@
-pub mod accum;
 pub mod gamm;
-pub mod ibcratelimit;
 pub mod incentives;
 pub mod lockup;
-pub mod mint;
-pub mod poolincentives;
 pub mod poolmanager;
-pub mod protorev;
 pub mod store;
 pub mod tokenfactory;
